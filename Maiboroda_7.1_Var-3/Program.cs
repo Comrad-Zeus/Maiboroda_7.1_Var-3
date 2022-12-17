f91abc285c206f8ae74a8ebba46ff5c54f97bd16
maybo <mayboroda2045@gmail.com>
2022-12-17 13:08:46 +0200

    Добавьте файлы проекта.

---
+++ Maiboroda_7.1_Var-3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Майборода А.А. - Вар 3 - Высокий
+var userString = Console.ReadLine().ToUpper().ToCharArray();
+
+var isPalindrome = true;
+for (var index = 0; index < userString.Length / 2; index++)
+{
+    if (userString[index] != userString[userString.Length - 1 - index])
+        isPalindrome = false;
+}
+
+Console.WriteLine(isPalindrome);
